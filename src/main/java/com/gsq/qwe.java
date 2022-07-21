53eb4a773388b42963aeabe7b0f003eafddc85d8
gsq <3075608633@qq.com>
2022-07-21 11:54:03 +0800

    git1

---
+++ src/main/java/com/gsq/qwe.java
@@ -0,0 +1,11 @@
+package com.gsq;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class qwe {
+    public static void main(String[] args) {
+        System.out.println("新建的git");
+    }
+
+
+}
